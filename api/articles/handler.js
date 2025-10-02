bb3e305fd0efda55074835efff6e138975f52280
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2025-10-02 20:43:05 +0700

    add column to table articles

---
+++ api/articles/handler.js
@@ -0,0 +1,9 @@
+class ArticlesHandler {
+    constructor(validator) {
+        this._validator = validator;
+    };
+
+    async addArticlesHandler(request, h) {
+        // validasi payload (title, )
+    };
+}
